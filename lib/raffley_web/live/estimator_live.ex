b7f2a9397b24a36deddc73c7f2039861ca46ce88
Albert Gabitov <agabitov@evrone.com>
2025-10-16 22:00:04 +0300

    L12: LiveView handle_info
    handling Elixir process messages

--- lib/raffley_web/live/estimator_live.ex
+++ lib/raffley_web/live/estimator_live.ex
@@ -2,6 +2,8 @@ defmodule RaffleyWeb.EstimatorLive do
   use RaffleyWeb, :live_view
 
   def mount(_params, _session, socket) do
+    if connected?(socket), do: send_tick()
+
     socket = assign(socket, tickets: 0, price: 3)
     {:ok, socket}
   end
@@ -42,5 +44,16 @@ defmodule RaffleyWeb.EstimatorLive do
     socket = assign(socket, :price, String.to_integer(price))
 
     {:noreply, socket}
+  end
+
+  def handle_info(:tick, socket) do
+    send_tick()
+    socket = update(socket, :tickets, &(&1 + 5))
+
+    {:noreply, socket}
+  end
+
+  defp send_tick() do
+    Process.send_after(self(), :tick, :timer.seconds(2))
   end
 end
