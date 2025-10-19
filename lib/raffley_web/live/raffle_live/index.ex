7f82520b55e4c785de4f1c97a66043467e4727ff
Albert Gabitov <agabitov@evrone.com>
2025-10-19 18:02:20 +0300

    L22: LiveView streams
    Changes the raffle listing to use LiveView streams. This enables more efficient updates and improves the perceived performance of the raffle list.

--- lib/raffley_web/live/raffle_live/index.ex
+++ lib/raffley_web/live/raffle_live/index.ex
@@ -6,7 +6,7 @@ defmodule RaffleyWeb.RaffleLive.Index do
   import RaffleyWeb.BannerComponents
 
   def mount(_params, _session, socket) do
-    socket = assign(socket, :raffles, Raffles.list_raffles())
+    socket = stream(socket, :raffles, Raffles.list_raffles())
     {:ok, socket}
   end
 
@@ -19,8 +19,8 @@ defmodule RaffleyWeb.RaffleLive.Index do
           <:details :let={emoji}>To Be Revealed Tomorrow {emoji}</:details>
           <:details>Any guesses?</:details>
         </.banner>
-        <div class="raffles">
-          <.raffle_card :for={raffle <- @raffles} raffle={raffle} />
+        <div class="raffles" id="raffles" phx-update="stream">
+          <.raffle_card :for={{dom_id, raffle} <- @streams.raffles} raffle={raffle} id={dom_id} />
         </div>
       </div>
     </Layouts.app>
@@ -28,10 +28,11 @@ defmodule RaffleyWeb.RaffleLive.Index do
   end
 
   attr :raffle, Raffley.Raffles.Raffle, required: true
+  attr :id, :string, required: true
 
   def raffle_card(assigns) do
     ~H"""
-    <.link navigate={~p"/raffles/#{@raffle}"}>
+    <.link navigate={~p"/raffles/#{@raffle}"} id={@id}>
       <div class="card">
         <img src={@raffle.image_path} />
         <h2>{@raffle.prize}</h2>
